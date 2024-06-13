f47f58618570ae805e7dab66ce03543b1832a5a1
Kethu <yerramma.kethu@adib.com>
2024-06-13 15:47:54 +0400

    Intrinsnic measurements

--- Practise/SampleApp/app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ Practise/SampleApp/app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -8,10 +8,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.sampleapp.ui.theme.SampleAppTheme
@@ -24,31 +34,36 @@ class MainActivity : ComponentActivity() {
         setContent {
             SampleAppTheme {
                 Scaffold {
-                    Card("Title","Description")
+                    TwoTexts(text1 = "Satya", text2 = "Kethu")
                 }
             }
         }
     }
+
     @Composable
-    fun Card(
-        title: String,
-        description: String
-    ) {
-        BoxWithConstraints {
-            if (maxWidth < 400.dp) {
-                Column {
-                    Image(painterResource(id = R.drawable.ic_launcher_background),contentDescription = null)
-                    Text(title)
-                }
-            } else {
-                Row {
-                    Column {
-                        Text(title)
-                        Text(description)
-                    }
-                    Image(painterResource(id = R.drawable.ic_launcher_background),contentDescription = null)
-                }
-            }
+    fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+        Row(modifier = modifier.height(IntrinsicSize.Min).padding(top=300.dp), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 4.dp)
+                    .wrapContentWidth(Alignment.Start),
+                text = text1
+            )
+            Divider(
+                color = Color.Black,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(1.dp)
+            )
+            Text(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 4.dp)
+                    .wrapContentWidth(Alignment.End),
+
+                text = text2
+            )
         }
     }
 }
