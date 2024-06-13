4734aa145aa427c4eb17552307d179769e0b65b0
Kethu <yerramma.kethu@adib.com>
2024-06-13 11:16:57 +0400

    Custom layout

--- Practise/SampleApp/app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ Practise/SampleApp/app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -5,43 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ContextualFlowRow
-import androidx.compose.foundation.layout.ContextualFlowRowOverflow
-import androidx.compose.foundation.layout.ContextualFlowRowOverflowScope
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowColumn
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.sampleapp.ui.theme.SampleAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,163 +26,75 @@ class MainActivity : ComponentActivity() {
         setContent {
             SampleAppTheme {
                 Scaffold {
-                    FlowColumnExample()
+                    CustomLayout()
                 }
             }
         }
     }
-}
 
-@OptIn(ExperimentalLayoutApi::class)
-@Composable
-fun FlowColumnExample() {
-    FlowColumn(
-        Modifier
-            .padding(20.dp)
-            .fillMaxHeight()
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        maxItemsInEachColumn = 3,
-    ) {
-        repeat(10) {
-            Box(
-                Modifier
-                    .fillMaxColumnWidth()
-                    .border(1.dp, Color.DarkGray, RoundedCornerShape(8.dp))
-                    .padding(8.dp)
-            ) {
-                Text(
-                    text = listOf("Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10")[it],
-                    fontSize = 18.sp,
-                    modifier = Modifier.padding(3.dp)
-                )
-            }
+    @Composable
+    private fun CustomLayout() {
+
+        MyBasicColumn(Modifier.padding(8.dp)) {
+            Text("Hi there!", Modifier.firstBaselineToTop(200.dp))
+            Text("MyBasicColumn")
+            Text("places items")
+            Text("vertically.")
+            Text("We've done it by hand!")
         }
     }
 
-}
 
-@OptIn(ExperimentalLayoutApi::class)
-@Composable
-fun GridFlow(){
-    Column {
-        val rows = 3
-        val columns = 3
-        FlowRow(
-            modifier = Modifier.padding(4.dp),
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            maxItemsInEachRow = rows
-        ) {
-            val itemModifier = Modifier
-                .padding(4.dp)
-                .height(80.dp)
-                .weight(1f)
-                .clip(RoundedCornerShape(8.dp))
-                .background(Color.Blue)
-            repeat(rows * columns) {
-                Spacer(modifier = itemModifier)
-            }
+    private fun Modifier.firstBaselineToTop(
+        firstBaselineToTop: Dp
+    ) = layout { measurable, constraints ->
+        // Measure the composable
+        val placeable = measurable.measure(constraints)
+
+        // Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // Height of the composable with padding - first baseline
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            // Where the composable gets placed
+            placeable.placeRelative(0, placeableY)
         }
+    }
 
-        FlowRow(
-            modifier = Modifier.padding(4.dp),
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            maxItemsInEachRow = 2
-        ) {
-            val itemModifier = Modifier
-                .padding(4.dp)
-                .height(80.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .background(Color.Blue)
-            repeat(6) { item ->
-                // if the item is the third item, don't use weight modifier, but rather fillMaxWidth
-                if ((item + 1) % 3 == 0) {
-                    Spacer(modifier = itemModifier.fillMaxWidth())
-                } else {
-                    Spacer(modifier = itemModifier.weight(0.5f))
+    @Composable
+    fun MyBasicColumn(
+        modifier: Modifier = Modifier,
+        content: @Composable () -> Unit
+    ) {
+        Layout(
+            modifier = modifier,
+            content = content
+        ) { measurables, constraints ->
+            // Don't constrain child views further, measure them with given constraints
+            // List of measured children
+            val placeables = measurables.map { measurable ->
+                // Measure each children
+                measurable.measure(constraints)
+            }
+
+            // Set the size of the layout as big as it can
+            layout(constraints.maxWidth, constraints.maxHeight) {
+                // Track the y co-ord we have placed children up to
+                var xPosition = 0
+
+                // Place children in the parent layout
+                placeables.forEach { placeable ->
+                    // Position item on the screen
+                    placeable.placeRelative(x = xPosition, y = 0)
+
+                    // Record the y co-ord placed up to
+                    xPosition += placeable.width
                 }
             }
         }
-
-        FlowRow(
-            modifier = Modifier.padding(4.dp),
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            maxItemsInEachRow = 3
-        ) {
-            val itemModifier = Modifier
-                .clip(RoundedCornerShape(8.dp))
-            Box(modifier = itemModifier.height(200.dp).width(60.dp).background(Color.Red))
-            Box(modifier = itemModifier.height(200.dp).fillMaxWidth(0.7f).background(Color.Blue))
-            Box(modifier = itemModifier.height(200.dp).weight(1f).background(Color.Magenta))
-        }
-    }
-}
-
-@OptIn(ExperimentalLayoutApi::class)
-@Composable
-fun ContextFlowEx() {
-    val totalCount = 40
-    var maxLines by remember { mutableStateOf(5) }
-
-    val moreOrCollapseIndicator = @Composable { scope: ContextualFlowRowOverflowScope ->
-        val remainingItems = totalCount - scope.shownItemCount
-        ChipItem(if (remainingItems == 0) "Less" else "+$remainingItems", onClick = {
-            if (remainingItems == 0) {
-                maxLines = 2
-            } else {
-                maxLines += 5
-            }
-        })
-    }
-    ContextualFlowRow(
-        modifier = Modifier
-            .safeDrawingPadding()
-            .fillMaxWidth(1f)
-            .padding(16.dp)
-            .wrapContentHeight(align = Alignment.Top)
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(4.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        maxLines = maxLines,
-        overflow = ContextualFlowRowOverflow.expandOrCollapseIndicator(
-            minRowsToShowCollapse = 4,
-            expandIndicator = moreOrCollapseIndicator,
-            collapseIndicator = moreOrCollapseIndicator
-        ),
-        itemCount = totalCount
-    ) { index ->
-        ChipItem("Item $index")
-    }
-}
-
-@OptIn(ExperimentalLayoutApi::class)
-@Composable
-private fun FlowRowSimpleUsageExample() {
-    FlowRow(modifier = Modifier.padding(16.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-        ChipItem("Price: High to Low")
-        ChipItem("Avg rating: 4+")
-        ChipItem("Free breakfast")
-        ChipItem("Free cancellation")
-        ChipItem("£50 pn")
-        ChipItem("Price: High to Low")
-        ChipItem("Avg rating: 4+")
-        ChipItem("Free breakfast")
-        ChipItem("Free cancellation")
-        ChipItem("£50 pn")
     }
 
 }
-
-@Composable
-fun ChipItem(text: String, onClick: (() -> Unit)? = null) {
-    Card(
-        modifier = Modifier.padding(end = 4.dp),
-        border = BorderStroke(1.dp, Color(0xFF3B3A3C))
-    ) {
-        Text(text, modifier = Modifier.padding(8.dp))
-    }
-}
-
-
-
